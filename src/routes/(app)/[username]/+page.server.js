5bd7a773ff5ce1133ccfb7db3a505b8d97de5351
watchinharrison <david.harrison@ladbiblegroup.com>
2023-01-10 13:42:58 +0000

    check profile exists

--- src/routes/(app)/[username]/+page.server.js
+++ src/routes/(app)/[username]/+page.server.js
@@ -21,40 +21,45 @@ export async function load({ locals, params, platform }) {
 		const postEntityRepo = new PostEntityRepository({ db: platform.env.DB });
 		const followersRepo = new FollowersRepository({ db: platform.env.DB });
 		const likesRepo = new LikesRepository({ db: platform.env.DB });
-		let posts = await postRepo.findByUsername(params.username);
 		const profile = await userRepo.findByUsername(params.username);
+		let posts = [];
+		let following = [];
+		let followers = [];
 		let isFollowing = false;
-		if (locals?.user?.id) {
-			isFollowing = await followersRepo.isFollowing({
-				followerId: locals.user.id,
-				followedId: profile.id
-			});
+		if (profile) {
+			posts = await postRepo.findByUsername(params.username);
+			if (locals?.user?.id) {
+				isFollowing = await followersRepo.isFollowing({
+					followerId: locals.user.id,
+					followedId: profile.id
+				});
+			}
+			following = await followersRepo.getFollowing({ userId: profile.id });
+			followers = await followersRepo.getFollowers({ userId: profile.id });
+			posts = await Promise.all(
+				posts.map(async (post) => {
+					post.entities = await postEntityRepo.findByPostId(post.id);
+					if (post.thread_id && !post.reply_id) {
+						const parentPost = await postRepo.findById(post.thread_id);
+						if (parentPost) {
+							post.thread = parentPost;
+							post.thread.liked = await likesRepo.getIsPostLiked({
+								postId: post.thread_id,
+								userId: locals.user?.id
+							});
+						}
+					}
+					if (post.reply_id) {
+						const parentPost = await postRepo.findById(post.reply_id);
+						if (parentPost) {
+							post.reply = parentPost;
+						}
+					}
+					post.liked = await likesRepo.getIsPostLiked({ postId: post.id, userId: locals.user?.id });
+					return post;
+				})
+			);
 		}
-		const following = await followersRepo.getFollowing({ userId: profile.id });
-		const followers = await followersRepo.getFollowers({ userId: profile.id });
-		posts = await Promise.all(
-			posts.map(async (post) => {
-				post.entities = await postEntityRepo.findByPostId(post.id);
-				if (post.thread_id && !post.reply_id) {
-					const parentPost = await postRepo.findById(post.thread_id);
-					if (parentPost) {
-						post.thread = parentPost;
-						post.thread.liked = await likesRepo.getIsPostLiked({
-							postId: post.thread_id,
-							userId: locals.user?.id
-						});
-					}
-				}
-				if (post.reply_id) {
-					const parentPost = await postRepo.findById(post.reply_id);
-					if (parentPost) {
-						post.reply = parentPost;
-					}
-				}
-				post.liked = await likesRepo.getIsPostLiked({ postId: post.id, userId: locals.user?.id });
-				return post;
-			})
-		);
 		return {
 			user: locals.user,
 			profile,
--- src/routes/(app)/[username]/+page.svelte
+++ src/routes/(app)/[username]/+page.svelte
@@ -6,5 +6,7 @@
 </script>
 
 <div class="max-w-[800px] m-auto">
-	<Profile {data} showEdit={true} user={data.user} />
+	{#if data?.profile}
+		<Profile {data} showEdit={true} user={data.user} />
+	{/if}
 </div>
